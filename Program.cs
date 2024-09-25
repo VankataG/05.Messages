435e623b307483acb23847c2b2ac01da7c745ed7
VankataG <djonatan2002@gmail.com>
2024-09-25 21:50:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int elements = nums.Length;
+
+            if (nums.Length > 1)
+
+            {
+                while (elements > 1)
+                {
+                    for (int i = 0; i < nums.Length - 1; i++)
+                    {
+                        nums[i] = nums[i] + nums[i + 1];
+                    }
+                    elements--;
+                }
+                Console.WriteLine(nums[0]);
+            }
+            else
+            {
+                Console.Write(nums[0]);
+            }
+        }
+    }
+}
